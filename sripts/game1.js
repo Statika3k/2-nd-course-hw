098320e8259103df84d192de5e898f69e0fe4191
Nina Shakhanova <shahanova.ns@gmail.com>
2025-05-20 18:50:47 +0300

    Добавлена первая игра

---
+++ sripts/game1.js
@@ -0,0 +1,25 @@
+document.getElementById('playGame-1').addEventListener('click',
+    function guessNumber() {
+        const secretNumber = Math.floor(Math.random() * 100) + 1;
+        let attempts = 0;
+
+        alert('Я загадал число от 1 до 100. Попробуй угадать!');
+
+
+
+        while (true) {
+            let guess = Number(prompt('Введи свой вариант:'));
+            attempts++;
+
+            if (guess === secretNumber) {
+                alert(`Поздравляю! Ты угадал число ${secretNumber} за ${attempts} попыток!`);
+                break;
+            } else if (guess < secretNumber) {
+                alert('Загаданное число больше. Попробуй еще!');
+            } else {
+                alert('Загаданное число меньше. Попробуй еще!');
+            }
+        }
+    }
+
+);
